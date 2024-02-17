6618ca2338256ca57f94470fb4515c0c7a1851c8
Erick Diaz <erickdiazsaavedra@gmail.com>
2024-02-17 09:03:59 -0500

    Implementación de sas y modelado examen final

---
+++ Examen Practico Final - FE1/Examen Practico Final - FE1/clase4.js
@@ -0,0 +1,19 @@
+// const caputurado = document.querySelector('h1');
+
+// const prueba = (caputurado) => {
+
+//     caputurado.innerHTML = 'Hola mundo'
+//     caputurado.style.color = 'black'
+// }
+
+// let retorno = prueba(caputurado)
+
+// let captura2 = document.querySelectorAll(".navegacion a");
+
+// console.log(captura2);
+// console.log(captura2[1]);
+
+// captura2.forEach(item => {
+//     item.style.color = 'red'
+//     item.style.textTransform = 'uppercase'
+// });
